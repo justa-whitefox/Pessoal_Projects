594c5454d1ecc273a7bd91934310b9228b8d0c04
justa-whitefox <justa.redfox2002@gmail.com>
2022-05-20 18:48:00 -0300

    adventure game

--- cpp/3.quizGame/Main.cpp
+++ cpp/3.quizGame/Main.cpp
@@ -35,21 +35,68 @@ queue<Question> LoadQuestions(){
 	Question q2 = Question("What is the chemical formula to table salt?", "NaC1", "NaC12", "Na2C1", 1);
 	Question q3 = Question("Which is the longest river in the world?", "nilo", "mississipi", "amazonas", 1);
 
-	queue<Question> Questions;
-	Questions.push(q1);
-	Questions.push(q2);
-	Questions.push(q3);
+	queue<Question> questions;
+	questions.push(q1);
+	questions.push(q2);
+	questions.push(q3);
 
-	return Questions;
+	return questions;
 }
 
 queue<Question> questions;
 Question currentQuestion;
+int selectedAnswer;
+int points = 0;
 
 TForm1 *Form1;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner): TForm(Owner){
-	questions = LoadQuestion();
+	questions = LoadQuestions();
 	currentQuestion = questions.front();
+	QuestionLabel->Text = currentQuestion.Text;
+
+	Answer1RadioButton->Text = currentQuestion.Answer1;
+	Answer2RadioButton->Text = currentQuestion.Answer2;
+	Answer3RadioButton->Text = currentQuestion.Answer3;
+
+	questions.pop();
+
+	PointsLabel->Text = points;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Answer1RadioButtonChange(TObject *Sender) {
+	selectedAnswer = 1;
+}
+
+void __fastcall TForm1::Answer2RadioButtonChange(TObject *Sender) {
+	selectedAnswer = 2;
+}
+
+void __fastcall TForm1::Answer3RadioButtonChange(TObject *Sender) {
+	selectedAnswer = 3;
+}
+
+void __fastcall TForm1::ConfirmButtonClick(TObject *Sender) {
+	if(selectedAnswer == currentQuestion.CorrectAnswer){
+		points++;
+        PointsLabel->Text = points;
+	}
+
+	if(!questions.empty()){
+		currentQuestion = questions.front();
+
+		QuestionLabel->Text = currentQuestion.Text;
+		Answer1RadioButton->Text = currentQuestion.Answer1;
+		Answer2RadioButton->Text = currentQuestion.Answer2;
+		Answer3RadioButton->Text = currentQuestion.Answer3;
+
+		questions.pop();
+		Answer1RadioButton->IsChecked = false;
+		Answer2RadioButton->IsChecked = false;
+        Answer3RadioButton->IsChecked = false;
+	} else {
+	  ConfirmButton->Enabled = false;
+      ConfirmButton->Text = "Quiz Ended";
+	}
 }
 //---------------------------------------------------------------------------
--- cpp/3.quizGame/Main.h
+++ cpp/3.quizGame/Main.h
@@ -10,16 +10,19 @@
 #include <FMX.StdCtrls.hpp>
 #include <FMX.Types.hpp>
 //---------------------------------------------------------------------------
-class TForm1 : public TForm
-{
+class TForm1 : public TForm {
 __published:	// IDE-managed Components
 	TRadioButton *Answer1RadioButton;
 	TRadioButton *Answer3RadioButton;
 	TRadioButton *Answer2RadioButton;
 	TLabel *QuestionLabel;
-	TButton *ConfirmBotton;
+	TButton *ConfirmButton;
 	TLabel *PointsLabel;
 	TLabel *Label3;
+	void __fastcall Answer1RadioButtonChange(TObject *Sender);
+	void __fastcall Answer2RadioButtonChange(TObject *Sender);
+	void __fastcall Answer3RadioButtonChange(TObject *Sender);
+	void __fastcall ConfirmButtonClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
